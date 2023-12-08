a217a6224e4a32fa0b28c4ffed882ce1bbf56dc2
Vladimir <vovar3573@mail.ru>
2023-12-08 17:24:47 +0300

    'HW6_Par_completed'

---
+++ Paradigm_HW/Sem6_Par/classwork/task003.py
@@ -0,0 +1,26 @@
+
+from typing import List
+
+
+def merge_sorting(lst: List[int]):
+    if len(lst) <= 1:
+        return lst
+    mid = len(lst) // 2
+    left = merge_sorting(lst[:mid])
+    right = merge_sorting(lst[mid:])
+    return merge(left, right)
+
+
+def merge(left: List[int], right: List[int]) -> List[int]:
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+    else:
+        result.append(right[j])
+        j += 1
+        return result + left[i:] + right[j:]
+
+print(merge_sorting([6,4,8,5,3,4,8,3,5]))
